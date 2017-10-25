e274e26bc0ddc2ca57866f5ab8c690dacf6bdc49
Hervé Péroteau <herve.peroteau@gmail.com>
2017-10-25 18:53:42 +0200

    Add MyKit

---
+++ TestPodCarthage/TestPodCarthage/MyKit.swift
@@ -0,0 +1,15 @@
+//
+//  MyKit.swift
+//  TestPodCarthage
+//
+//  Created by Hervé PEROTEAU on 25/10/2017.
+//  Copyright © 2017 Hervé PEROTEAU. All rights reserved.
+//
+
+import Foundation
+
+public class MyKit {
+    public func toto() {
+        print("toto ok")
+    }
+}
