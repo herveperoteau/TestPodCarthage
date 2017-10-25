5170e77f396f90722a039722e8ac005dbb794c2a
Hervé Péroteau <herve.peroteau@gmail.com>
2017-10-25 18:47:48 +0200

    Starter project

---
+++ TestPodCarthage/TestPodCarthage/TestPodCarthage.h
@@ -0,0 +1,19 @@
+//
+//  TestPodCarthage.h
+//  TestPodCarthage
+//
+//  Created by Hervé PEROTEAU on 25/10/2017.
+//  Copyright © 2017 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestPodCarthage.
+FOUNDATION_EXPORT double TestPodCarthageVersionNumber;
+
+//! Project version string for TestPodCarthage.
+FOUNDATION_EXPORT const unsigned char TestPodCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodCarthage/PublicHeader.h>
+
+
